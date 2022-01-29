554d3c4ff062b68a13a04a6b55eecd742c59ec40
Adrian <23638074+sysadmin-info@users.noreply.github.com>
2022-01-30 00:32:22 +0100

    Added some comments for better explanation

--- nodejs-npm-pm2-install.sh
+++ nodejs-npm-pm2-install.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
 #################################################################################
-#Script Name	: nodejs-npm-pm2-install.sh
-#Description	: script that installs nodejs, npm and pm2 on a Raspbery Pi 4b  
-#Author       	: Adrian Ambroziak
-#Email         	: adrian.ambroziak@gmail.com
+# Script Name: nodejs-npm-pm2-install.sh
+# Description: script that installs nodejs, npm and pm2 on a Raspbery Pi 4b  
+# Author:      Adrian Ambroziak
+# Email:       adrian.ambroziak@gmail.com
 #################################################################################
 
+# NVM (Node Version Manager) is a bash script that allows you to install and manage multiple Node.js versions. 
+# Use this method if you need to install a specific Node.js version or if you need to have more than one Node.js versions installed on your Raspberry Pi.
+# To install nvm run the following curl command which will download and run the nvm installation script:
+
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+
+# run the following to use nvm now:
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+# Read the explanation about version installation: https://linuxize.com/post/how-to-install-node-js-on-raspberry-pi/
 
 #install the long term supported version
 nvm install --lts
@@ -58,6 +66,10 @@ sudo systemctl start pm2-pi.service
 
 #enable the pm2 service during boot
 sudo systemctl enable pm2-pi.service
+
+#Install development tools
+#To be able to compile and install native add-ons from the npm registry you need to install the development tools:
+sudo apt install build-essential
 
 # recommended reboot after the installation
 sudo reboot
